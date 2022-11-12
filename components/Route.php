c01c47c159a222b727f6354c4f5a73f7fed5504d
fadi.hijazi <fadihijazi2018@outlook.com>
2022-11-12 14:39:22 +0200

    implement another way to fetch `REQUEST_URI` from register `ROUTE` with support dynamic params passing to controllers.

--- components/Route.php
+++ components/Route.php
@@ -12,8 +12,60 @@ class Route
             throw new Exception("Route::$method, `$method` isn't supported.");
         }
 
-        require_once "controllers/$controller.php";
-        self::$_ROUTES[$uri][$method] = $controller::$method();
+        self::$_ROUTES[$uri][$method] = $controller;
+    }
+
+    /**
+     * @param array $uri_parts
+     * @return array
+     */
+    private static function getUriWithParams(array $uri_parts): array
+    {
+        /*
+        array structure like
+            [
+                'uri' => "",
+                'params' => []
+            ]
+        array can be empty if not route is found.
+         */
+        foreach (array_keys(self::$_ROUTES) as $route)
+        {
+            $registered_uri_parts = explode("/", $route);
+            $params = [];
+            $break_loop = false;
+
+            if (sizeof($registered_uri_parts) != sizeof($uri_parts))
+            {
+                continue;
+            }
+
+            foreach ($registered_uri_parts as $index => $registered_uri_part)
+            {
+                // handle {.whatever} as param.
+                if (preg_match("/^{.*}$/", $registered_uri_part))
+                {
+                    $params[] = $uri_parts[$index];
+                }
+                elseif ($registered_uri_part != $uri_parts[$index])
+                {
+                    // reset params
+                    $params = [];
+                    $break_loop = true;
+                    break;
+                }
+            }
+
+            if (! $break_loop)
+            {
+                return [
+                    'uri' => $route,
+                    'params' => $params
+                ];
+            }
+        }
+
+        return [];
     }
 
     public static function GET($uri, $controller)
@@ -28,33 +80,37 @@ class Route
 
     public static function handleRequest()
     {
-        $path =
-            join(
-                "/",
-                array_slice(explode('/', $_SERVER['REQUEST_URI']), 2)
-            );
-        $method = $_SERVER['REQUEST_METHOD'];
+        $request_uri = array_slice(explode('/', $_SERVER['REQUEST_URI']), 2);
+        $request_method = $_SERVER['REQUEST_METHOD'];
 
-        if (! key_exists($path, self::$_ROUTES))
+        $uri_params = self::getUriWithParams($request_uri);
+
+        $uri = $uri_params['uri'] ?? "";
+        $params = $uri_params['params'] ?? "";
+
+        if (! $uri_params || ! key_exists($uri, self::$_ROUTES))
         {
             $response = [
                 'message' => "not found.",
                 'status_code' => StatusCodes::NOT_FOUND
             ];
-            http_response_code(StatusCodes::NOT_FOUND);
         }
-        elseif(! key_exists($method, self::$_ROUTES[$path]))
+        elseif(! key_exists($request_method, self::$_ROUTES[$uri]))
         {
             $response = [
-                'message' => "method `$method` not supported for this path.",
+                'message' => "$request_method request method isn't supported for this uri.",
                 'status_code' => StatusCodes::METHOD_NOT_ALLOWED
             ];
-            http_response_code(StatusCodes::METHOD_NOT_ALLOWED);
         }
         else
         {
-            $response = self::$_ROUTES[$path][$method];
+            $controller = self::$_ROUTES[$uri][$request_method];
+            require_once "controllers/$controller.php";
+
+            $response = (new $controller())->$request_method(... $params);
         }
+
+        http_response_code($response['status_code']);
 
         return json_encode($response);
     }
