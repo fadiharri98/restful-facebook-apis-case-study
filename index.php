426bb3616130d3ae686a59ee216eb868c8517135
fadi.hijazi <fadihijazi2018@outlook.com>
2022-11-12 14:39:50 +0200

    import `BaseController` in `index.php` instead of each of controller

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 header('Content-Type: application/json; charset=utf-8');
 
+require_once "controllers/BaseController.php";
 require_once "constants/StatusCodes.php";
 require_once "components/Route.php";
 
