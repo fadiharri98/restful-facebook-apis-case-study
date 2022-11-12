d036bf660c970ec120997bd1caabac2033a08d29
fadi.hijazi <fadihijazi2018@outlook.com>
2022-11-12 14:37:36 +0200

    edit `BaseController` & sub controllers structure to be - their methods not static, - implement `__call` magic method to validate `response` structure for all sub controllers, - import `BaseController` in `index.php` once, then no need to import again on sub controllers - support accept dynamic & implement that in `PostController`.`get` with validation - set success status code through `StatusCodes` not direct like previous

--- controllers/BaseController.php
+++ controllers/BaseController.php
@@ -2,6 +2,23 @@
 
 abstract class BaseController
 {
-    public static abstract function get();
-    public static abstract function post();
+    protected abstract function get();
+    protected abstract function post();
+
+    public function __call($name, $arguments)
+    {
+        $response = $this->$name(...$arguments);
+
+        // validate response
+        if (! array_key_exists('data', $response))
+        {
+            throw new Exception("response should contains data associated with `data` key.");
+        }
+        elseif (! array_key_exists('status_code', $response))
+        {
+            throw new Exception("response should contains a status code associated with `status_code` key.");
+        }
+
+        return $response;
+    }
 }
--- controllers/PostController.php
+++ controllers/PostController.php
@@ -1,10 +1,29 @@
 <?php
-require_once __DIR__ . "/BaseController.php";
 
 class PostController extends BaseController
 {
-    public static function get()
+    protected function get()
     {
+        list($user_id,) = func_get_args();
+
+        // expect id, if there is no id, then validation exception response
+        if (! $user_id)
+        {
+            return [
+                'data' => 'id should passed within uri.',
+                'status_code' => StatusCodes::VALIDATION_ERROR
+            ];
+        }
+        elseif (! is_numeric($user_id))
+        {
+            return [
+                'data' => 'id should be integer.',
+                'status_code' => StatusCodes::VALIDATION_ERROR
+            ];
+        } else {
+            $user_id = intval($user_id);
+        }
+
         // posts data from database
         return [
             'data' => [
@@ -12,33 +31,33 @@ class PostController extends BaseController
                     'title' => "Foo",
                     'description' => "foo description",
                     'created' => "25/11/2020 09:12:55",
-                    'user_id' => 1
+                    'user_id' => $user_id
                 ],
                 [
                     'title' => "Foo foo",
                     'description' => "foo x2 description",
                     'created' => "25/11/2020 09:19:25",
-                    'user_id' => 1
+                    'user_id' => $user_id
                 ],
                 [
                     'title' => "Bar",
                     'description' => "bar description",
                     'created' => "25/11/2020 09:14:15",
-                    'user_id' => 2
+                    'user_id' => $user_id
                 ]
             ],
-            'status_code' => 200
+            'status_code' => StatusCodes::SUCCESS
         ];
     }
 
-    public static function post()
+    protected function post()
     {
         // some actions
         return [
             'data' => [
                 'message' => "Success"
             ],
-            'status_code' => 200
+            'status_code' => StatusCodes::SUCCESS
         ];
     }
 }
