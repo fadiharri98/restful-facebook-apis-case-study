5d4f02929d5d2d1f3f829aec3fdaa4c592e7c51d
fadi.hijazi <fadihijazi2018@outlook.com>
2022-11-12 14:40:16 +0200

    new uris for `like` resource.

--- routes/v1/urls.php
+++ routes/v1/urls.php
@@ -4,5 +4,8 @@ $api_v1 = "api/v1";
 Route::GET("$api_v1/users", UserController::class);
 Route::POST("$api_v1/users", UserController::class);
 
-Route::GET("$api_v1/users/%s/posts", PostController::class);
-Route::POST("$api_v1/users/%s/posts", PostController::class);
+Route::GET("$api_v1/users/{user_id}/posts", PostController::class);
+Route::POST("$api_v1/users/{user_id}/posts", PostController::class);
+
+Route::GET("$api_v1/posts/{post_id}/likes", LikeController::class);
+Route::POST("$api_v1/posts/{post_id}/likes", LikeController::class);
