ea1841da1fcfc8c7c83d03cb36c17a5112919a0d
fadi.hijazi <fadihijazi2018@outlook.com>
2022-11-12 14:37:48 +0200

    new `LikeController`

---
+++ controllers/LikeController.php
@@ -0,0 +1,60 @@
+<?php
+
+class LikeController extends BaseController
+{
+    protected function get()
+    {
+        list($post_id,) = func_get_args();
+
+        // expect id, if there is no id, then validation exception response
+        if (! $post_id)
+        {
+            return [
+                'data' => 'id should passed within uri.',
+                'status_code' => StatusCodes::VALIDATION_ERROR
+            ];
+        }
+        elseif (! is_numeric($post_id))
+        {
+            return [
+                'data' => 'id should be integer.',
+                'status_code' => StatusCodes::VALIDATION_ERROR
+            ];
+        } else {
+            $post_id = intval($post_id);
+        }
+
+        // posts data from database
+        return [
+            'data' => [
+                [
+                    'user_id' => rand(1000, 5000),
+                    'post_id' => $post_id,
+                    'created' => "25/11/2020 09:12:55",
+                ],
+                [
+                    'user_id' => rand(1000, 5000),
+                    'post_id' => $post_id,
+                    'created' => "25/11/2020 09:12:55",
+                ],
+                [
+                    'user_id' => rand(1000, 5000),
+                    'post_id' => $post_id,
+                    'created' => "25/11/2020 09:12:55",
+                ],
+            ],
+            'status_code' => StatusCodes::SUCCESS
+        ];
+    }
+
+    protected function post()
+    {
+        // some actions
+        return [
+            'data' => [
+                'message' => "[LikeController] Success"
+            ],
+            'status_code' => StatusCodes::SUCCESS
+        ];
+    }
+}
