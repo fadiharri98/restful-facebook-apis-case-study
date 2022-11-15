ac9aa507c53de51581b51e903898e7425fc287a7
fadi.hijazi <fadihijazi2018@outlook.com>
2022-11-15 20:46:47 +0200

    update index according the added namespaces.

--- index.php
+++ index.php
@@ -15,10 +15,13 @@ Dotenv::createImmutable(__DIR__)->safeLoad();
  */
 header('Content-Type: application/json; charset=utf-8');
 
-require_once "controllers/BaseController.php";
-require_once "constants/StatusCodes.php";
-require_once "components/Route.php";
-
+/*
+ * register all app APIs (according to RESTFUL-API standards)
+ */
 require_once "routes/v1/urls.php";
 
+/*
+ * handle coming request via Route component
+ */
+use Components\Route;
 echo Route::handleRequest();
