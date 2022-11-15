7edf6834ac679f3045d58c71bdc731a34c210a4c
fadi.hijazi <fadihijazi2018@outlook.com>
2022-11-15 19:18:42 +0200

    update uri to be url on all `Route` class.

--- components/Route.php
+++ components/Route.php
@@ -6,50 +6,50 @@ class Route
 
     private static $_ROUTES = [];
 
-    private static function register($method, $uri, $controller)
+    private static function register($method, $url, $controller)
     {
         if (! in_array($method, self::SUPPORTED_METHODS)) {
             throw new Exception("Route::$method, `$method` isn't supported.");
         }
 
-        self::$_ROUTES[$uri][$method] = $controller;
+        self::$_ROUTES[$url][$method] = $controller;
     }
 
     /**
-     * @param array $uri_parts
+     * @param array $url_parts
      * @return array
      */
-    private static function mapUriWithParams(array $uri_parts): array
+    private static function mapUrlWithParams(array $url_parts): array
     {
         /*
-         * mapping target route (as Uri) & detecting its route-params with passed uri.
+         * mapping target route (as Url) & detecting its route-params with passed url.
          * if mapping failed, an empty array will be the result.
         array structure like
             [
-                'uri' => "",
+                'url' => "",
                 'params' => []
             ]
         array can be empty if not route is found.
          */
         foreach (array_keys(self::$_ROUTES) as $route)
         {
-            $registered_uri_parts = explode("/", $route);
+            $registered_url_parts = explode("/", $route);
             $params = [];
             $break_loop = false;
 
-            if (sizeof($registered_uri_parts) != sizeof($uri_parts))
+            if (sizeof($registered_url_parts) != sizeof($url_parts))
             {
                 continue;
             }
 
-            foreach ($registered_uri_parts as $index => $registered_uri_part)
+            foreach ($registered_url_parts as $index => $registered_url_part)
             {
                 // handle {.whatever} as param.
-                if (preg_match("/^{.*}$/", $registered_uri_part))
+                if (preg_match("/^{.*}$/", $registered_url_part))
                 {
-                    $params[] = $uri_parts[$index];
+                    $params[] = $url_parts[$index];
                 }
-                elseif ($registered_uri_part != $uri_parts[$index])
+                elseif ($registered_url_part != $url_parts[$index])
                 {
                     // reset params
                     $params = [];
@@ -61,7 +61,7 @@ class Route
             if (! $break_loop)
             {
                 return [
-                    'uri' => $route,
+                    'url' => $route,
                     'params' => $params
                 ];
             }
@@ -70,43 +70,43 @@ class Route
         return [];
     }
 
-    public static function GET($uri, $controller)
+    public static function GET($url, $controller)
     {
-        self::register('GET', $uri, $controller);
+        self::register('GET', $url, $controller);
     }
 
-    public static function POST($uri, $controller)
+    public static function POST($url, $controller)
     {
-        self::register('POST', $uri, $controller);
+        self::register('POST', $url, $controller);
     }
 
     public static function handleRequest()
     {
-        $request_uri_parts = array_slice(explode('/', $_SERVER['REQUEST_URI']), 2);
+        $request_url_parts = array_slice(explode('/', $_SERVER['REQUEST_URI']), 2);
         $request_method = $_SERVER['REQUEST_METHOD'];
 
-        $uri_params = self::mapUriWithParams($request_uri_parts);
+        $url_params = self::mapUrlWithParams($request_url_parts);
 
-        $uri = $uri_params['uri'] ?? "";
-        $params = $uri_params['params'] ?? "";
+        $url = $url_params['url'] ?? "";
+        $params = $url_params['params'] ?? "";
 
-        if (! $uri_params || ! key_exists($uri, self::$_ROUTES))
+        if (! $url_params || ! key_exists($url, self::$_ROUTES))
         {
             $response = [
                 'message' => "not found.",
                 'status_code' => StatusCodes::NOT_FOUND
             ];
         }
-        elseif(! key_exists($request_method, self::$_ROUTES[$uri]))
+        elseif(! key_exists($request_method, self::$_ROUTES[$url]))
         {
             $response = [
-                'message' => "$request_method request method isn't supported for this uri.",
+                'message' => "$request_method request method isn't supported for this url.",
                 'status_code' => StatusCodes::METHOD_NOT_ALLOWED
             ];
         }
         else
         {
-            $controller = self::$_ROUTES[$uri][$request_method];
+            $controller = self::$_ROUTES[$url][$request_method];
             require_once "controllers/$controller.php";
 
             $response = (new $controller())->$request_method(... $params);
