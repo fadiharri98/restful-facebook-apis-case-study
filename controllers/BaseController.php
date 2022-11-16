ef684bb26113b05215a0287e95a031e1ee7e35f7
fadi.hijazi <fadihijazi2018@outlook.com>
2022-11-16 15:20:13 +0200

    update `BaseController`.`post` & get as none static abstract methods, add default response property, & add new `put`, `delete` methods.

--- controllers/BaseController.php
+++ controllers/BaseController.php
@@ -1,18 +1,63 @@
 <?php
 namespace Controllers;
 
+use Constants\StatusCodes;
 use Exception;
 
 abstract class BaseController
 {
-    protected abstract function get();
-    protected abstract function post();
+    /**
+     * The default representing the structure of expected response
+     * @var array
+     */
+    private array $defaultResponse = [
+        'data' => '',
+        'status_code' => StatusCodes::SUCCESS
+    ];
+
+    /**
+     * Should override by subclass to provide custom behavior
+     * @return array
+     */
+    protected function get(): array
+    {
+        return $this->defaultResponse;
+    }
+
+    /**
+     * Should override by subclass to provide custom behavior
+     * @return array
+     */
+    protected function post(): array
+    {
+        return $this->defaultResponse;
+    }
+
+    /**
+     * Should override by subclass to provide custom behavior
+     * @return array
+     */
+    protected function put(): array
+    {
+        return $this->defaultResponse;
+    }
+
+    /**
+     * Should override by subclass to provide custom behavior
+     * @return array
+     */
+    protected function delete(): array
+    {
+        return $this->defaultResponse;
+    }
 
     public function __call($name, $arguments)
     {
         $response = $this->$name(...$arguments);
 
-        // validate response
+        /**
+         * validation: response should always has `data` & `status_code`
+         */
         if (! array_key_exists('data', $response))
         {
             throw new Exception("response should contains data associated with `data` key.");
