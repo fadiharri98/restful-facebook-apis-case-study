19424432ef0d64cf38274993a54c156e59f630ec
fadi.hijazi <fadihijazi2018@outlook.com>
2022-11-17 11:32:38 +0200

    cleanup (all cleaned codes has been written for testing purposes).

--- routes/v1/urls.php
+++ routes/v1/urls.php
@@ -1,19 +1,10 @@
 <?php
+/**
+ * Script to register all app URLs.
+ * Register URLs by using `Route` component.
+ * All URLs in this script are in version 1, so should have suffix "api/v1".
+ */
 use Components\Route;
-use Controllers\UserController;
-use Controllers\PostController;
-use Controllers\LikeController;
 
 $api_v1 = "api/v1";
 
-Route::GET("$api_v1/users", UserController::class);
-Route::POST("$api_v1/users", UserController::class);
-
-Route::GET("$api_v1/users/{user_id}/posts", PostController::class);
-Route::POST("$api_v1/users/{user_id}/posts", PostController::class);
-Route::GET("$api_v1/users/{user_id}/posts/{post_id}", PostController::class, "show");
-Route::PUT("$api_v1/users/{user_id}/posts/{post_id}", PostController::class);
-Route::DELETE("$api_v1/users/{user_id}/posts/{post_id}", PostController::class);
-
-Route::GET("$api_v1/posts/{post_id}/likes", LikeController::class);
-Route::POST("$api_v1/posts/{post_id}/likes", LikeController::class);
