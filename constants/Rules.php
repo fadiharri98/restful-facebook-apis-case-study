dbf528f1857a8e0f8b138c5c5e337934b9d9aad8
fadi.hijazi <fadihijazi2018@outlook.com>
2022-11-20 22:41:17 +0200

    new constants.Rules.

---
+++ constants/Rules.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Constants;
+
+class Rules extends BaseConstant
+{
+    const REQUIRED = 'required';
+    const NOT_EMPTY = 'string_not_empty';
+    const INTEGER = 'integer';
+    const STRING = 'string';
+    const BOOLEAN = 'boolean';
+    const EMAIL = 'email';
+    const MAX = 'MAX';
+    const MIN = 'MIN';
+    const LENGTH = 'length';
+}
