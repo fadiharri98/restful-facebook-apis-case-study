ef218677f1cfcc2d957f313c6b0b4f66acfc638b
fadi.hijazi <fadihijazi2018@outlook.com>
2022-11-26 06:40:55 +0200

    new serializers.

---
+++ serializers/CommentSerializer.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Serializers;
+
+use Models\Comment;
+
+class CommentSerializer extends BaseSerializer
+{
+    protected $mapFieldWithCustomName = [
+        'content' => 'comment'
+    ];
+
+    /**
+     * @return bool
+     */
+    public function validModel($model = null)
+    {
+        return ($model ?: $this->model) instanceof Comment;
+
+    }
+
+    /**
+     * @return string[]
+     */
+    public function getSerializationFields()
+    {
+        return [
+            'id',
+            'content',
+            'user:id,username,profile_img',
+            'created'
+        ];
+
+    }
+
+}
