9270a70aa89d6b1ff4b386e72eed2532c32b4098
fadi.hijazi <fadihijazi2018@outlook.com>
2022-12-11 08:37:46 +0200

    cleanups, extract actual structure.

--- routes/v1/urls.php
+++ routes/v1/urls.php
@@ -5,38 +5,3 @@
  * All URLs in this script are in version 1, so should have suffix "api/v1".
  */
 use Components\Route;
-
-use Controllers\UserController;
-use Controllers\PostController;
-use Controllers\CommentController;
-
-use Nested\Controllers\UserPostController;
-use Nested\Controllers\PostCommentController;
-use Nested\Controllers\PostLikeController;
-
-$api_v1 = "api/v1";
-
-Route::GET("$api_v1/users", UserController::class);
-Route::GET("$api_v1/users/{user_id}", UserController::class, "show");
-Route::POST("$api_v1/users", UserController::class);
-Route::PUT("$api_v1/users/{user_id}", UserController::class);
-Route::DELETE("$api_v1/users/{user_id}", UserController::class);
-
-Route::GET("$api_v1/posts/{post_id}", PostController::class, "show");
-Route::POST("$api_v1/posts", PostController::class);
-Route::PUT("$api_v1/posts/{post_id}", PostController::class);
-Route::DELETE("$api_v1/posts/{post_id}", PostController::class);
-
-Route::GET("$api_v1/users/{user_id}/posts", UserPostController::class);
-
-Route::GET("$api_v1/posts/{post_id}/likes", PostLikeController::class);
-
-Route::POST("$api_v1/posts/{post_id}/like", PostController::class, "likeThePost");
-Route::POST("$api_v1/posts/{post_id}/unlike", PostController::class, "unlikeThePost");
-
-Route::GET("$api_v1/posts/{post_id}/comments", PostCommentController::class);
-Route::POST("$api_v1/posts/{post_id}/comments", PostCommentController::class);
-
-Route::PUT("$api_v1/comments/{comment_id}", CommentController::class);
-Route::DELETE("$api_v1/comments/{comment_id}", CommentController::class);
-
